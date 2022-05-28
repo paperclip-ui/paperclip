0d3e000a3ffc00471d62e4a6d5a8a5736f8a9461
Craig Condon <craig.j.condon@gmail.com>
2022-05-28 18:38:29 -0400

    ask to save before closing

--- packages/@tandem-ui/designer/src/engines/global.ts
+++ packages/@tandem-ui/designer/src/engines/global.ts
@@ -3,10 +3,13 @@ import { Action } from "redux";
 import { RootState } from "../state";
 import { Dispatch } from "react";
 import { rootClicked } from "../actions";
+import { isDirty } from "fsbox";
 
 export const globalsEngine = (
   dispatch: Dispatch<Action>
 ): EngineActionHandler<RootState> => {
+  let _currentState: RootState;
+
   document.addEventListener(
     "wheel",
     (event: WheelEvent) => {
@@ -33,7 +36,18 @@ export const globalsEngine = (
     alert(state.confirm.message);
   };
 
+  window.onbeforeunload = () => {
+    if (!_currentState) {
+      return;
+    }
+    if (Object.values(_currentState.fileCache).some(isDirty)) {
+      return `You have unsaved files. Are you sure you want to leave?`;
+    }
+  };
+
   return (action: Action, state: RootState, prevState: RootState) => {
+    _currentState = state;
+
     handleConfirm(action, state, prevState);
   };
 };

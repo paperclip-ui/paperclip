a7cc30c68ad52724328061418b31a0dae1784e1b
Craig Condon <craig.j.condon@gmail.com>
2022-08-13 11:47:58 +0100

    start on async

--- packages/@paperclip-lang/core/crates/engine/src/graph/graph.rs
+++ packages/@paperclip-lang/core/crates/engine/src/graph/graph.rs
@@ -1,6 +1,7 @@
 use crate::config::config::Config;
 use crate::pc::ast::Document;
 use crate::pc::parser::parse as parse_pc;
+use futures::future;
 use std::collections::HashMap;
 
 pub trait IO {
@@ -26,17 +27,22 @@ impl<'io, TIO: IO> Graph<'io, TIO> {
             io,
         }
     }
-    pub fn load(&mut self, path: &String) {
+    pub async fn load(&mut self, path: &String) {
         if self.dependencies.contains_key(path) {
             return;
         }
 
         if let Ok(document) = parse_pc(self.io.read(path).as_str(), path) {
             let mut imports: HashMap<String, String> = HashMap::new();
+
             for import in &document.get_imports() {
                 let import_path = self.io.resolve(path, &import.path);
                 imports.insert(import.path.to_string(), import_path);
             }
+
+            self.load_imports(&imports).await;
+
+          
 
             self.dependencies.insert(
                 path.to_string(),
@@ -48,4 +54,17 @@ impl<'io, TIO: IO> Graph<'io, TIO> {
             );
         }
     }
+
+
+    async fn load_imports(&mut self, imports: &HashMap<String, String>) {
+
+      let mut loads = vec![];
+
+      for (_, path) in imports {
+        loads.push(self.load(path));
+      }
+
+      future::select_all(loads).await;
+    }    
 }
+

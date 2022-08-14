07b060fac5c67a3fdadd1d749dc977f19e0efbf5
Craig Condon <craig.j.condon@gmail.com>
2022-08-14 23:54:48 +0100

    start on cli

--- packages/@paperclip-lang/core/crates/cli/src/main.rs
+++ packages/@paperclip-lang/core/crates/cli/src/main.rs
@@ -1,3 +1,18 @@
+use clap::Parser;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+   /// Name of the person to greet
+   #[clap(short, long, value_parser)]
+   name: String,
+
+   /// Number of times to greet
+   #[clap(short, long, value_parser, default_value_t = 1)]
+   count: u8,
+}
+
 fn main() {
     println!("Hello, world!");
 }

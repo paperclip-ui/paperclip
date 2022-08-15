6d538010b8be0d08d45f5f415873c0ebeb211783
Craig Condon <craig.j.condon@gmail.com>
2022-08-15 16:11:20 +0100

    add cli command

--- packages/@paperclip-lang/core/crates/cli/src/main.rs
+++ packages/@paperclip-lang/core/crates/cli/src/main.rs
@@ -1,20 +1,71 @@
-use clap::Parser;
+// https://github.com/clap-rs/clap/blob/495e49e1a70989f4c8904c355f90d6149f673ce2/examples/git-derive.rs
+// https://paperclip.dev/docs/usage-cli
+
+use clap::{Parser, Subcommand};
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
-   /// Name of the person to greet
-   #[clap(short, long, value_parser)]
-   name: String,
 
-   /// Number of times to greet
-   #[clap(short, long, value_parser, default_value_t = 1)]
-   count: u8,
+    #[clap(subcommand)]
+   command: Command,
+
 }
 
+
+#[derive(Subcommand, Debug)]
+enum Command {
+
+    /// Builds a paperclip project
+    #[clap(arg_required_else_help = false)]
+    Build {
+        /// Prints the compiled output 
+        #[clap(short, long, value_parser, default_value_t = true)]
+        print: bool,
+
+        /// Starts the file watcher
+        #[clap(short, long, value_parser, default_value_t = false)]
+        watch: bool,
+
+        /// The config file to use for compiling
+        #[clap(short, long)]
+        config: Option<String>
+    },
+
+    /// Configures Paperclip with your current project & installs compilers.
+    #[clap(arg_required_else_help = false)]
+    Init {
+
+    },
+
+    /// Starts the visual development tooling
+    #[clap(arg_required_else_help = false)]
+    Dev {
+
+    }
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    match args.command {
+        Command::Build { print, watch, config } => {
+            println!("DDDDDD");
+        },
+        Command::Init { } => {
+
+        },
+        Command::Dev {} => {
+
+        }
+    };
+
+ 
+    // for _ in 0..args.count {
+    //     println!("Hello {}!", args.name)
+    // }
 }
 
 #[cfg(test)]

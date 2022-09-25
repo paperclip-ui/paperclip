c8b19b0cb65fd964a045e39a58810de9af0ddf5f
Craig Condon <craig.j.condon@gmail.com>
2022-09-25 12:42:40 -0500

    include client

--- libs/vscode-extension/src/extension/language/server/designer-client.ts
+++ libs/vscode-extension/src/extension/language/server/designer-client.ts
@@ -1,5 +1,13 @@
-export class DesignerClient {
-  constructor() {}
+import { DesignerClient as GRPCDesignerClient } from "@paperclip-ui/proto/lib/service/designer_grpc_web_pb";
 
+export class DesignerClient {
+  private _client: GRPCDesignerClient;
+  constructor() {}
+  start() {
+    // 1. look for open designer
+    // 2. if no open designer, then look for VS Code binary
+    //   a. download binary from releases based on VS Code extension
+    // 3. Connect to local GRPC server
+  }
   updateVirtualFileContent(filePath: string, text: string) {}
 }

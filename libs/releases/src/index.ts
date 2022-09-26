d7a6893530d08f15b41c4e14de2d8e22de2c03a9
Craig Condon <craig.j.condon@gmail.com>
2022-09-25 20:38:57 -0500

    change tar type

--- libs/releases/src/index.ts
+++ libs/releases/src/index.ts
@@ -1,12 +1,72 @@
-import fetch from "node-fetch";
-const pkg = require("../../package.json");
+import got from "got";
+import fs from "fs";
+import fsa from "fs-extra";
+import * as os from "node:os";
+const pkg = require("../package.json");
+import path from "path";
 
-export const downloadRelease = async () => {
-  const releases = await fetch(
-    `https://api.github.com/repos/paperclip-ui/paperclip/releases`
-  ).then((response) => response.json());
+// paperclip_test2_x86_64-pc-windows-gnu.zip
 
-  console.log(releases);
+const OS_VENDOR = {
+  win32: "windows",
+  darwin: "apple",
+  linux: "linux",
+}[os.platform as any];
+
+export const downloadRelease = async (releaseDir: string) => {
+  const currentVersion = extractVersionParts(pkg.version);
+  const releases = (await got
+    .get(`https://api.github.com/repos/paperclip-ui/paperclip/releases`)
+    .json()) as any[];
+
+  const matchingRelease =
+    releases
+      .filter((release) => {
+        const releaseVersion = extractVersionParts(release.tag_name);
+        return (
+          releaseVersion.major === currentVersion.major &&
+          release.minor == releaseVersion.minor
+        );
+      })
+      .sort((a, b) => {
+        return Number(extractVersionParts(a.tag_name).patch) >
+          Number(extractVersionParts(b.tag_name).patch)
+          ? -1
+          : 1;
+      })[0] || releases[0];
+
+  if (!matchingRelease) {
+    throw new Error(
+      `No matching release for ${currentVersion.major}.${currentVersion.major}`
+    );
+  }
+
+  const asset = matchingRelease.assets.find((asset) => {
+    return asset.name.includes(OS_VENDOR);
+  });
+
+  if (!asset) {
+    throw new Error(`${os.platform} is not currently supported`);
+  }
+
+  try {
+    fsa.mkdirSync(releaseDir);
+  } catch (e) {}
+  const zipPath = path.join(
+    releaseDir,
+    path.basename(asset.browser_download_url)
+  );
+
+  (await got.get(asset.browser_download_url)).pipe(
+    fs.createWriteStream(zipPath)
+  );
+
+  console.log(zipPath);
 };
 
-downloadRelease();
+const extractVersionParts = (version: string) => {
+  const [major, minor, patch] = version.split(".");
+  return { major, minor, patch };
+};
+
+downloadRelease("/tmp/releases");

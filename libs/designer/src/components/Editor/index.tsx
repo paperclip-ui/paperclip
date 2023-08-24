0ba9d15b1f385676b8ead92c1d2f8010581a23c3
Craig Condon <craig.j.condon@gmail.com>
2023-08-24 16:23:02 -0500

    add splash

--- libs/designer/src/components/Editor/index.tsx
+++ libs/designer/src/components/Editor/index.tsx
@@ -5,16 +5,26 @@ import { RightSidebar } from "./EditorPanels/RightSidebar";
 import { LeftSidebar } from "./EditorPanels/LeftSidebar";
 import { CenterPanels } from "./EditorPanels/Center";
 import { ResourceModal } from "./ResourceModal";
+import { useSelector } from "@paperclip-ui/common";
+import { getCurrentFilePath } from "../../state";
 
 export const Editor = () => {
+  const currentFile = useSelector(getCurrentFilePath);
+
   return (
     <styles.Editor>
       <ResourceModal />
       <Canvas />
       <styles.EditorPanels>
         <LeftSidebar />
-        <CenterPanels />
-        <RightSidebar />
+        {currentFile ? (
+          <>
+            <CenterPanels />
+            <RightSidebar />
+          </>
+        ) : (
+          <styles.SplashInfo />
+        )}
       </styles.EditorPanels>
     </styles.Editor>
   );

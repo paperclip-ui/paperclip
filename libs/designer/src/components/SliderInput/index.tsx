e784d80ce3723ad47a4ffc69076c764909c79b46
Craig Condon <craig.j.condon@gmail.com>
2023-09-02 15:28:09 -0500

    add slider input

---
+++ libs/designer/src/components/SliderInput/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import * as styles from "@paperclip-ui/designer/src/styles/slider-input.pc";
+
+export const SliderInput = () => {
+  const dotStyle = { left: `10%` };
+  return <styles.Slider dotStyle={dotStyle}></styles.Slider>;
+};

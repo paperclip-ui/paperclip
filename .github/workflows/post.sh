2d36e316e9800af7a4a47b32dd82896285a89650
Craig Condon <craig.j.condon@gmail.com>
2023-09-03 14:38:33 -0500

    include

--- .github/workflows/post.sh
+++ .github/workflows/post.sh
@@ -1,11 +1,33 @@
 #!/bin/bash
 
-# https://localazy.com/blog/how-to-automatically-sign-macos-apps-using-github-actions
-echo $MACOS_CERTIFICATE | base64 --decode > certificate.p12
-security create-keychain -p $MACOS_CERTIFICATE_PWD build.keychain
-security default-keychain -s build.keychain
-security unlock-keychain -p $MACOS_CERTIFICATE_PWD build.keychain
-security import certificate.p12 -k build.keychain -P $MACOS_CERTIFICATE_PWD -T /usr/bin/codesign
-security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k $MACOS_CERTIFICATE_PWD build.keychain
+# https://docs.github.com/en/actions/deployment/deploying-xcode-applications/installing-an-apple-certificate-on-macos-runners-for-xcode-development
+
+
+#!/bin/bash
+
+# https://docs.github.com/en/actions/deployment/deploying-xcode-applications/installing-an-apple-certificate-on-macos-runners-for-xcode-development
+
+# create variables
+CERTIFICATE_PATH=$RUNNER_TEMP/build_certificate.p12
+PP_PATH=$RUNNER_TEMP/build_pp.mobileprovision
+KEYCHAIN_PATH=$RUNNER_TEMP/app-signing.keychain-db
+
+# import certificate and provisioning profile from secrets
+echo -n "$BUILD_CERTIFICATE_BASE64" | base64 --decode -o $CERTIFICATE_PATH
+
+# create temporary keychain
+security create-keychain -p "$KEYCHAIN_PASSWORD" $KEYCHAIN_PATH
+security set-keychain-settings -lut 21600 $KEYCHAIN_PATH
+security unlock-keychain -p "$KEYCHAIN_PASSWORD" $KEYCHAIN_PATH
+
+# import certificate to keychain
+security import $CERTIFICATE_PATH -P "$P12_PASSWORD" -A -t cert -f pkcs12 -k $KEYCHAIN_PATH
+security list-keychain -d user -s $KEYCHAIN_PATH
+
+# apply provisioning profile
+# mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
+# cp $PP_PATH ~/Library/MobileDevice/Provisioning\ Profiles
+
+security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k "$P12_PASSWORD" build.keychain
 ID=$(security find-identity -v)
 /usr/bin/codesign --force -s $ID ./target/release/paperclip-cli -v

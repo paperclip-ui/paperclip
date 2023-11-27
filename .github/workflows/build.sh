f4d4665bc6e3899c43e78f7100da94d8d00415c4
Craig Condon <craig.j.condon@gmail.com>
2023-11-27 19:49:32 +0700

    clean up

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -1,9 +1,7 @@
 #!/bin/sh -l
 
 yarn install
-echo $GITHUB_REPOSITORY
-cd $GITHUB_REPOSITORY
-run ls
+yarn build
 cargo build --release --locked --target x86_64-unknown-linux-musl
 
 echo "BIN_FILE=target/release/paperclip_cli" >> "$GITHUB_OUTPUT"

4537085558ae53b65fc82ae3403c2a5ad74f25b1
Craig Condon <craig.j.condon@gmail.com>
2023-11-28 11:16:59 +0700

    touch to rerun

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -6,3 +6,4 @@ cargo build --release --locked --target x86_64-unknown-linux-musl
 find target -maxdepth 3
 
 echo "BIN_FILE=target/x86_64-unknown-linux-musl/release/paperclip_cli" >> "$GITHUB_OUTPUT"
+
